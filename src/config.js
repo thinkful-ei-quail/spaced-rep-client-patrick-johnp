e55db112261fcc7f7069e22db800fdc2a98417cc
John <jpending@gmail.com>
2020-10-06 17:41:33 -0400

    more styling

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://enigmatic-woodland-60186.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
 //http://localhost:8000/api
